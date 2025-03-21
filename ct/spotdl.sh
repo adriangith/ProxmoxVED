f6794db9767f9b8ce38a2bb15b5dd6b2b9831ffc
Adrian Zafir <adrian.au.gm@gmail.com>
2025-03-21 00:02:29 +0000

    Fixed testing repository links

--- ct/spotdl.sh
+++ ct/spotdl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://github.com/adriangith/ProxmoxVED/refs/heads/spotDL/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/adriangith/ProxmoxVED/refs/heads/spotDL/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: adriangith
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
