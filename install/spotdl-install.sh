8a557b788ea95cea2b3a3afd502489d64a43bb6d
Adrian Zafir <adrian.au.gm@gmail.com>
2025-03-21 02:58:28 +0000

    Debugging spotdl link

--- install/spotdl-install.sh
+++ install/spotdl-install.sh
@@ -29,7 +29,7 @@ msg_ok "Installed Dependencies"
 msg_info "Setup ${APPLICATION}"
 RELEASE=$(curl -s https://api.github.com/repos/spotdl/spotify-downloader/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 wget -q "https://github.com/spotDL/spotify-downloader/releases/download/${RELEASE}/${APPLICATION}-${RELEASE#v}-linux"
-mv "${APPLICATION}-${RELEASE#v}-linux" "/opt/spotify-downloader/${APPLICATION}"
+mv -p "${APPLICATION}-${RELEASE#v}-linux" "/opt/spotify-downloader/${APPLICATION}"
 chmod +x /opt/spotify-downloader/"${APPLICATION}"
 #
 #
